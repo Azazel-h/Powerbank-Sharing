3fe94b864dc215966adcab84a8dc14562e436a29
Dmitrij Kondratev <dim.kondr@yandex.ru>
2019-05-04 12:17:31 +0300

    Refactor

---
+++ sharing/tests/__init__.py
@@ -0,0 +1,10 @@
+"""
+Тесты всех страниц
+"""
+from sharing.tests.user import *
+from sharing.tests.pages import *
+from sharing.tests.create_powerbank import *
+from sharing.tests.cheat import *
+from sharing.tests.order_powerbank import *
+from sharing.tests.scan import *
+from sharing.tests.wallet import *
