4ffc8bc0a5921e82c52db65859cdfee166e8ca48
Azazel <arseniy2003bulgakov@gmail.com>
2019-05-11 13:16:11 +0300

    One more

--- sharing/tests/helpers.py
+++ sharing/tests/helpers.py
@@ -14,15 +14,17 @@ class TestHelpers(TestCase):
         user = User.objects.create_user(username='Azazel',
                                         email='azazel@ya.ru',
                                         password='23452345')
-        Profile.objects.create(user=user, name='azazel')
+        p1 = Profile.objects.create(user=user, name='azazel')
 
         superuser = User.objects.create_superuser(username='root',
                                                   email='root@ya.ru',
                                                   password='rootpass')
-        Profile.objects.create(user=superuser, name='root')
+        p2 = Profile.objects.create(user=superuser, name='root')
 
         pb = Powerbank.objects.create(capacity=1000,location=1,status='free')
 
     def test_powerbank_percentage(self):
         self.assertEqual(1, powerbank_percentage())
 
+    def test_get_profile(self):
+        self.assertEqual(get_profile(self.user), self.p1)
