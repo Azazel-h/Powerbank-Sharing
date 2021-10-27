84c456648c35304dbe55c6feb2cb7b2423a84271
Azazel_h <arseniy2003bulgakov@gmail.com>
2021-10-27 09:02:12 +0300

    Another time

--- power_bank/settings.py
+++ power_bank/settings.py
@@ -129,10 +129,8 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/sharing/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'sharing/static/'),
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'sharing/static/'),
-]
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
