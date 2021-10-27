f7c10eaa39d0674fa8dd48a6421144aae3b8a193
Azazel_h <arseniy2003bulgakov@gmail.com>
2021-10-27 08:54:52 +0300

    Static bug fix

--- power_bank/settings.py
+++ power_bank/settings.py
@@ -128,7 +128,7 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/sharing/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
